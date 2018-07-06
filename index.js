cbcaf3399dadbb988e22f3025b42578959a2728b
paulotozzi <p.tozzi2016@gmail.comm>
2018-07-05 21:36:04 -0300

    api restfull com nodejs e mysql

---
+++ index.js
@@ -0,0 +1,45 @@
+const restify = require('restify');
+
+const errs = require('restify-errors');
+const PORT = process.env.PORT || 3000;
+const HOST = '127.0.0.1';
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : 'sql10.freemysqlhosting.net',
+      user : 'sql10246290',
+      password : 'p9M16wVRTv',
+      database : 'sql10246290'
+    }
+  });
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.get('/cidades', (req, res, next) => {
+    knex('vwcidades').then((dados) => {
+        res.send(dados);
+    }, next)
+});
+
+server.get('/categorias', (req, res, next) => {
+    knex('vwcategorias').then((dados) => {
+        res.send(dados);
+    }, next)
+});
+
+server.get('/estabelecimentos', (req, res, next) => {
+    knex('vwestabelecimentos').then((dados) => {
+        res.send(dados);
+    }, next)
+});
+
+
+server.listen(PORT);
+console.log(`Running on port:${PORT}`);
